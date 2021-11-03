d2c5f321c3f1bda3921d6c9781784ddfa54d1434
Pedroca <pedro.henric.rosario@gmail.com>
2021-11-03 14:05:26 -0300

    Camada Model

---
+++ CadastroExemplo-Asp-Net-Mvc/CadastroExemplo-Asp-Net-Mvc/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroExemplo_Asp_Net_Mvc.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public DateTime DataPublicacao { get; set; }
+
+        public Post()
+        {
+            DataPublicacao = DateTime.Now;
+        }
+    }
+}
